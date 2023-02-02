19a9a39d8b83f5f2f7c43931e9c846ef2d1c0880
Anurav-Garg <anurav.grg@gmail.com>
2023-02-02 21:58:26 +0530

    Basic index file setup

---
+++ index.ts
@@ -0,0 +1,64 @@
+import express, { application } from "express";
+import { Request, Response, NextFunction } from "express";
+import bodyParser from "body-parser";
+import session from "express-session";
+import { createClient } from "redis";
+import connectRedis from "connect-redis";
+import { RedisStore } from "connect-redis";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const app: express.Application = express();
+const port: number = 3000;
+const redisStore: RedisStore = connectRedis(session);
+const redisClient = createClient({
+  legacyMode: true, // Apparently needed for connect-redis to work right
+  socket: { port: 6379 },
+});
+
+redisClient.on("error", function (err) {
+  console.log("Could not establish a connection with redis. " + err);
+});
+redisClient.on("connect", function (err) {
+  console.log("Connected to redis successfully");
+});
+redisClient.connect();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  session({
+    store: new redisStore({ client: redisClient }),
+    secret: process.env.SECRET_KEY as string,
+    resave: false, // As redis implements the touch method (I think) this should be unnecessary
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 1,
+    },
+  })
+);
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message:
+      "Hello, welcome to the Calendar Lender API. One day when documentation for it exists, this endpoint will lead to it.",
+  });
+});
+
+// app.get("/test", (req: Request, res: Response) => {
+//   const session = req.session;
+//   if (!session.count) {
+//     session.count = 1;
+//     res.status(200).json({ message: "This is your first time visiting!" });
+//   } else {
+//     session.count++;
+//     res
+//       .status(200)
+//       .json({ message: `You have visited us ${session.count} times!` });
+//   }
+// });
+
+app.listen(port, () => {
+  console.log("Server started on port " + port);
+});
