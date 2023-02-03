3cafd213f8d1654adb14312eb37f24fc12721968
Anurav-Garg <anurav.grg@gmail.com>
2023-02-03 17:46:04 +0530

    Created get user details function

---
+++ routes/users/getUserDetails.ts
@@ -0,0 +1,27 @@
+import { User } from "@prisma/client";
+import { Request, Response } from "express";
+import prisma from "../../prisma/client";
+
+export default async function (req: Request, res: Response) {
+  if (!req.session.auth) {
+    res
+      .status(401)
+      .json({ message: "User not logged in, or session was timed out." });
+    return;
+  }
+
+  const user: User | null = await prisma.user.findUnique({
+    where: {
+      username: req.session.auth.username,
+    },
+  });
+
+  if (!user) {
+    res.status(500).json({ message: "Something went wrong" });
+    return;
+  }
+
+  res
+    .status(200)
+    .json({ username: user.username, email: user.email, name: user.name });
+}
