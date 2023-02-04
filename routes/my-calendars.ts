a2cb323ddbb7863a2941a0efb7306200ecdea8c5
Anurav-Garg <anurav.grg@gmail.com>
2023-02-04 11:04:27 +0530

    Created router for /my-calendars

---
+++ routes/my-calendars.ts
@@ -0,0 +1,44 @@
+import express, { NextFunction, Request, Response } from "express";
+import prisma from "../prisma/client";
+import {
+  createCalendar,
+  getAllCalendars,
+  calendarIdRouter,
+} from "./my-calendars/index";
+import { default as auth } from "./_authenticate";
+
+const router: express.Router = express.Router();
+
+router.use(auth);
+
+router.post("/", createCalendar);
+
+router.get("/", getAllCalendars);
+
+router.use("/:calendarId", authorize, calendarIdRouter);
+
+async function authorize(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> {
+  req.calendarId = req.params.calendarId;
+
+  const calendar = await prisma.calendar.findUnique({
+    where: { id: req.calendarId },
+  });
+
+  if (!calendar) {
+    res.status(400).json({ message: "Invalid calendar ID" });
+    return;
+  }
+
+  if (calendar.ownerUsername !== req.session.auth?.username) {
+    res.status(403).json({ message: "User does not own this calendar" });
+    return;
+  }
+
+  next();
+}
+
+export default router;
