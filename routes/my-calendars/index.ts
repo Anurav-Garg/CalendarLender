4ca00f1074f3454a35081258b72c0930268a1e7b
Anurav-Garg <anurav.grg@gmail.com>
2023-02-04 11:08:47 +0530

    Created calendar-id router

--- routes/my-calendars/index.ts
+++ routes/my-calendars/index.ts
@@ -1,2 +1,19 @@
+import express from "express";
+import {
+  deleteCalendar,
+  getCalendarDetails,
+  renameCalendar,
+  eventRouter,
+} from "./calendar-id/index";
 export { default as createCalendar } from "./createCalendar";
 export { default as getAllCalendars } from "./getAllCalendars";
+
+const calendarIdRouter: express.Router = express.Router();
+
+calendarIdRouter.get("/", getCalendarDetails);
+calendarIdRouter.delete("/", deleteCalendar);
+calendarIdRouter.patch("/", renameCalendar);
+
+calendarIdRouter.use("/events", eventRouter);
+
+export { calendarIdRouter };
