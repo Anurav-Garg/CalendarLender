bd5afdc59124b9643da150917f1c720155d6d2b5
Anurav-Garg <anurav.grg@gmail.com>
2023-02-04 11:08:07 +0530

    Created index route for events

---
+++ routes/my-calendars/calendar-id/events/index.ts
@@ -0,0 +1,4 @@
+export { default as createEvent } from "./createEvent";
+export { default as getAllEvents } from "./getAllEvents";
+export { default as deleteEvent } from "./deleteEvent";
+export { default as updateEvent } from "./updateEvent";
