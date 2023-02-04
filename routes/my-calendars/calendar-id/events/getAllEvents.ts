cb59d7d865ac7739f3b6a81f6712694b42c1e56b
Anurav-Garg <anurav.grg@gmail.com>
2023-02-04 11:07:49 +0530

    Created get all events route

---
+++ routes/my-calendars/calendar-id/events/getAllEvents.ts
@@ -0,0 +1,11 @@
+import { CalendarEvent } from "@prisma/client";
+import { Request, Response } from "express";
+import prisma from "../../../../prisma/client";
+
+export default async function (req: Request, res: Response) {
+  const events: CalendarEvent[] = await prisma.calendarEvent.findMany({
+    where: { calendarId: req.calendarId },
+  });
+
+  res.status(200).json(events);
+}
