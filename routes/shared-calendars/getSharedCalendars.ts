4646a28035fdae1a21367fb6d7ecad5f5fd922a7
Anurav-Garg <anurav.grg@gmail.com>
2023-02-05 16:01:08 +0530

    Added getSharedCalendars function

---
+++ routes/shared-calendars/getSharedCalendars.ts
@@ -0,0 +1,19 @@
+import { Calendar } from "@prisma/client";
+import { Request, Response } from "express";
+import { prisma } from "../../lib/initializeClients";
+
+export default async function (req: Request, res: Response) {
+  const username: string = req.username as string;
+
+  const calendars: Calendar[] = await prisma.calendar.findMany({
+    where: {
+      sharedTo: {
+        some: {
+          username: username,
+        },
+      },
+    },
+  });
+
+  res.status(200).json(calendars);
+}
